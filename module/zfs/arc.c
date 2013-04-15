00f6e7cdbce9fe6cce22f81f71ee8ebdb78ffc80
Bjoern Kahl <googlelogin@bjoern-kahl.de>
2013-04-15 02:13:35 +0200

    Partial revert of 3cbd04e to make libzpool work.  Needs review if this is the right approach.

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -2113,7 +2113,6 @@ arc_shrink(uint64_t bytes)
 		arc_adjust();
 }
 
-#if 0
 static void
 arc_kmem_reap_now(arc_reclaim_strategy_t strat, uint64_t bytes)
 {
@@ -2144,7 +2143,7 @@ arc_kmem_reap_now(arc_reclaim_strategy_t strat, uint64_t bytes)
 	kmem_cache_reap_now(buf_cache);
 	kmem_cache_reap_now(hdr_cache);
 }
-#endif
+
 /*
  * Unlike other ZFS implementations this thread is only responsible for
  * adapting the target ARC size on Linux.  The responsibility for memory
