f120f56702670c28bb698f8056bfabdc4476027f
Andrey Vesnovaty <andrey.vesnovaty@gmail.com>
2013-07-30 12:59:34 +0300

    Fix invalid fileid for snapshot root dentry
    Prevents NFS client from detection of different fileids of snapshot root dentry
    before & after snapshot mount.
    
    Signed-off-by: Andrey Vesnovaty <andrey.vesnovaty@gmail.com>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -3197,6 +3197,22 @@ zfs_getattr_fast(struct inode *ip, struct kstat *sp)
 		 */
 		sp->blksize = zsb->z_max_blksz;
 	}
+
+	mutex_exit(&zp->z_lock);
+
+	/*
+	 * Required to prevent NFS client from detecting different inode
+	 * numbers of snapshot root dentry before and after snapshot mount.
+	 */
+	if (zsb->z_issnap) {
+		if (ip->i_sb->s_root->d_inode == ip)
+			sp->ino = ZFSCTL_INO_SNAPDIRS -
+				dmu_objset_id(zsb->z_os);
+	}
+
+	ZFS_EXIT(zsb);
+
+	return (0);
 }
 #endif
 
