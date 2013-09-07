35e081d4756ba9559f4a97feba27135c15be012a
Jorgen Lundman <lundman@lundman.net>
2013-09-07 16:42:37 +0900

    Add simple kext load script

---
+++ load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pids=`ps -axww|grep "[t]ail -f /var/log/system.log" | awk '{print $1;}'`
+if [ x"$pids" != x"" ]; then
+    kill $pids
+fi
+tail -f /var/log/system.log &
+
+rsync -ar module/zfs/zfs.kext/ /tmp/zfs.kext/
+rsync -ar ../spl/module/spl/spl.kext/ /tmp/spl.kext/
+
+chown -R root:wheel /tmp/spl.kext /tmp/zfs.kext
+
+kextload -v /tmp/spl.kext
+
+kextload -v -r /tmp/spl.kext /tmp/zfs.kext
