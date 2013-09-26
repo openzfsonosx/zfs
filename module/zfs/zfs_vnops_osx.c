46e05484f6c97ab031f2364f895b7ebd8b845f7e
Jorgen Lundman <lundman@lundman.net>
2013-09-26 15:35:53 +0900

    We need to reply to PC_XATTR_SIZE_BITS in pathconf for SMB sharing. Fix rare vdev_is_dead(NULL) panic.

--- module/zfs/zfs_vnops_osx.c
+++ module/zfs/zfs_vnops_osx.c
@@ -491,6 +491,8 @@ zfs_vnop_fsync(
 
     zfsvfs = zp->z_zfsvfs;
 
+    if (!zfsvfs) return 0;
+
     /*
      * Because vnode_create() can end up calling fsync, which means we would
      * sit around waiting for dmu_tx, while higher up in this thread may
@@ -1387,6 +1389,7 @@ zfs_vnop_pathconf(
                 *valp = 1;
                 break;
 
+        case _PC_XATTR_SIZE_BITS:
         case _PC_FILESIZEBITS:
                 *valp = 64;
                 break;
