fe60c44bbfb3df8ef9446c9d68a4cbd42134d7c5
zAdmin <admin@flatmac2.flanet>
2014-03-03 17:38:17 +1100

    Maintain C99 style for variable declarations, and conditionally compile linux variables out.

--- module/zfs/vdev_queue.c
+++ module/zfs/vdev_queue.c
@@ -383,14 +383,15 @@ vdev_queue_fini(vdev_t *vd)
 static void
 vdev_queue_io_add(vdev_queue_t *vq, zio_t *zio)
 {
+#ifdef LINUX
+	spa_t *spa = zio->io_spa;
+	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
+#endif
 
 	ASSERT3U(zio->io_priority, <, ZIO_PRIORITY_NUM_QUEUEABLE);
 	avl_add(&vq->vq_class[zio->io_priority].vqc_queued_tree, zio);
 
 #ifdef LINUX
-	spa_t *spa = zio->io_spa;
-	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
-	
     if (ssh->kstat != NULL) {
 		mutex_enter(&ssh->lock);
 		kstat_waitq_enter(ssh->kstat->ks_data);
@@ -402,13 +403,15 @@ vdev_queue_io_add(vdev_queue_t *vq, zio_t *zio)
 static void
 vdev_queue_io_remove(vdev_queue_t *vq, zio_t *zio)
 {
+#ifdef LINUX
+	spa_t *spa = zio->io_spa;
+	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
+#endif
 
 	ASSERT3U(zio->io_priority, <, ZIO_PRIORITY_NUM_QUEUEABLE);
 	avl_remove(&vq->vq_class[zio->io_priority].vqc_queued_tree, zio);
 
 #ifdef LINUX
-	spa_t *spa = zio->io_spa;
-	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
 	if (ssh->kstat != NULL) {
 		mutex_enter(&ssh->lock);
 		kstat_waitq_exit(ssh->kstat->ks_data);
@@ -421,14 +424,17 @@ static void
 vdev_queue_pending_add(vdev_queue_t *vq, zio_t *zio)
 {
 
+#ifdef LINUX
+	spa_t *spa = zio->io_spa;
+	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
+#endif
+
 	ASSERT(MUTEX_HELD(&vq->vq_lock));
 	ASSERT3U(zio->io_priority, <, ZIO_PRIORITY_NUM_QUEUEABLE);
 	vq->vq_class[zio->io_priority].vqc_active++;
 	avl_add(&vq->vq_active_tree, zio);
 
 #ifdef LINUX
-	spa_t *spa = zio->io_spa;
-	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
 	if (ssh->kstat != NULL) {
 		mutex_enter(&ssh->lock);
 		kstat_runq_enter(ssh->kstat->ks_data);
@@ -440,6 +446,10 @@ vdev_queue_pending_add(vdev_queue_t *vq, zio_t *zio)
 static void
 vdev_queue_pending_remove(vdev_queue_t *vq, zio_t *zio)
 {
+#ifdef LINUX
+	spa_t *spa = zio->io_spa;
+	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
+#endif
 
 	ASSERT(MUTEX_HELD(&vq->vq_lock));
 	ASSERT3U(zio->io_priority, <, ZIO_PRIORITY_NUM_QUEUEABLE);
@@ -447,8 +457,6 @@ vdev_queue_pending_remove(vdev_queue_t *vq, zio_t *zio)
 	avl_remove(&vq->vq_active_tree, zio);
 
 #ifdef LINUX
-	spa_t *spa = zio->io_spa;
-	spa_stats_history_t *ssh = &spa->spa_stats.io_history;
 	if (ssh->kstat != NULL) {
 		kstat_io_t *ksio = ssh->kstat->ks_data;
 
