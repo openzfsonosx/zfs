3a2a8767a8e2ba83a6d0104637b8caa3d1bde1cb
Jorgen Lundman <lundman@lundman.net>
2014-04-17 12:00:16 +0900

    config.remove event added for ZED

--- include/sys/fm/fs/zfs.h
+++ include/sys/fm/fs/zfs.h
@@ -37,6 +37,7 @@ extern "C" {
 #define	FM_EREPORT_ZFS_DATA			"data"
 #define	FM_EREPORT_ZFS_DELAY			"delay"
 #define	FM_EREPORT_ZFS_CONFIG_SYNC		"config.sync"
+#define	FM_EREPORT_ZFS_CONFIG_REMOVE	"config.remove"
 #define	FM_EREPORT_ZFS_POOL			"zpool"
 #define	FM_EREPORT_ZFS_POOL_DESTROY		"zpool.destroy"
 #define	FM_EREPORT_ZFS_POOL_REGUID		"zpool.reguid"
