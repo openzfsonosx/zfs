dc11abb1602467cee4b59a63ea3e7e2a8646025f
Gerhard Roethlin <git@the-color-black.net>
2014-04-27 16:11:42 +0200

    Readme and License
    This program is licensed under a "3-Clause BSD" license.

--- InvariantDisks/IDDiskArbitrationLogger.hpp
+++ InvariantDisks/IDDiskArbitrationLogger.hpp
@@ -2,8 +2,11 @@
 //  IDDiskArbitrationLogger.hpp
 //  InvariantDisks
 //
-//  Created by cbreak on 2014.04.27.
+//  Created by Gerhard Röthlin on 2014.04.27.
 //  Copyright (c) 2014 the-color-black.net. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted
+//  provided that the conditions of the "3-Clause BSD" license described in the LICENSE file are met.
 //
 
 #ifndef ID_DISKARBITRATIONLOGGER_HPP
---
+++ README.md
@@ -0,0 +1,10 @@
+IndependentDisks
+================
+
+InvariantDisks is a small program maintaining a mapping from invariant labels to the potentially
+varying /dev/disk* entries.
+
+License
+=======
+
+This program is licensed under the "3-clause BSD" License. See the LICENSE.md file for details.
