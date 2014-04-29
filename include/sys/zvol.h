770e28d25c2bdc8952c4aa1e5684fda678a3bb87
ilovezfs <ilovezfs@icloud.com>
2014-04-29 02:56:40 -0700

    Second arg to zfs_ereport_zvol_post is const char * not spa_t *

--- include/sys/zvol.h
+++ include/sys/zvol.h
@@ -130,7 +130,7 @@ extern int      zvolCreateNewDevice(zvol_state_t *zv);
 extern int      zvolSetVolsize(zvol_state_t *zv);
 
 extern int zvol_busy(void);
-extern void zfs_ereport_zvol_post(const char *subclass, spa_t *spa,
+extern void zfs_ereport_zvol_post(const char *subclass, const char *name,
                                   const char *bsd, const char *rbsd);
 
 
