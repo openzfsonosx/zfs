e928cf9a7aab009032180652ef39b645e293ff66
ilovezfs <ilovezfs@icloud.com>
2014-06-30 22:15:14 +0900

    Clean up zfs_util.c whitespace and memory management

--- cmd/zfs_util/zfs_util.c
+++ cmd/zfs_util/zfs_util.c
@@ -1,30 +1,34 @@
 /*
-* Copyright (c) 2007 Apple Inc. All rights reserved.
-* Use is subject to license terms stated below.
-*
-* The contents of this file are subject to the terms of the
-* Common Development and Distribution License v. 1.0 (the "License").
-* You may not use this file except in compliance with the License.
-*
-* You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-* or http://www.opensolaris.org/os/licensing <http://www.opensolaris.org/os/licensing> .
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-* THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-*THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*	@(#)zfsutil.c   (c) 2007 Apple Inc.	*/
+ * Copyright (c) 2007 Apple Inc. All rights reserved.
+ * Use is subject to license terms stated below.
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <syslog.h>
 #include <libzfs.h>
@@ -55,7 +59,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <syslog.h>
-#define COMMON_DIGEST_FOR_OPENSSL
+#define	COMMON_DIGEST_FOR_OPENSSL
 #include <CommonCrypto/CommonDigest.h>
 #include <uuid/uuid.h>
 
@@ -67,7 +71,7 @@ usage(void)
 {
 	fprintf(stderr, "usage: %s -p device\n", progname);
 	closelog();
-        exit(FSUR_INVAL);
+	exit(FSUR_INVAL);
 }
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
@@ -75,74 +79,77 @@ usage(void)
 #include <IOKit/storage/IOMedia.h>
 #include <DiskArbitration/DiskArbitration.h>
 
-
 #ifndef FSUC_GETUUID
-#define FSUC_GETUUID 'k'
+#define	FSUC_GETUUID 'k'
 #endif
 
 #ifndef FSUC_QUICKVERIFY
-#define FSUC_QUICKVERIFY 'q'
+#define	FSUC_QUICKVERIFY 'q'
 #endif
 
-
-
 static int
-zfs_probe(const char *devpath, char **volname)
+zfs_probe(const char *devpath, io_name_t volname)
 {
 	int result = FSUR_UNRECOGNIZED;
-	CFMutableDictionaryRef  matchingDict;
-    io_service_t            service;
+	CFMutableDictionaryRef matchingDict;
+	io_service_t service;
+	CFStringRef cfstr;
 
-	if (!strncmp("/dev/", devpath, 5))
+	if (strncmp("/dev/", devpath, 5) == 0)
 		devpath = &devpath[5];
 
 	syslog(LOG_NOTICE, "+zfs_probe : devpath %s", devpath);
 
 	matchingDict = IOBSDNameMatching(kIOMasterPortDefault, 0, devpath);
-    if (NULL == matchingDict) {
-			syslog(LOG_NOTICE, "IOBSDNameMatching returned a NULL dictionary.\n");
-    } else {
-        // Fetch the object with the matching BSD node name.
-        // Note that there should only be one match, so IOServiceGetMatchingService is used instead of
-        // IOServiceGetMatchingServices to simplify the code.
-        service = IOServiceGetMatchingService(kIOMasterPortDefault, matchingDict);
+	if (NULL == matchingDict) {
+		syslog(LOG_NOTICE, "IOBSDNameMatching returned a NULL "
+		    "dictionary.\n");
+	} else {
+		/*
+		 * Fetch the object with the matching BSD node name.
+		 * Note that there should only be one match, so
+		 * IOServiceGetMatchingService is used instead of
+		 * IOServiceGetMatchingServices to simplify the code.
+		 */
+		service = IOServiceGetMatchingService(kIOMasterPortDefault,
+		    matchingDict);
 
-        if (IO_OBJECT_NULL == service) {
-			syslog(LOG_NOTICE, "IOServiceGetMatchingService returned IO_OBJECT_NULL.\n");
-        } else {
+		if (IO_OBJECT_NULL == service) {
+			syslog(LOG_NOTICE, "IOServiceGetMatchingService "
+			    "returned IO_OBJECT_NULL.\n");
+		} else {
 			syslog(LOG_NOTICE, "writing\n");
 			if (IOObjectConformsTo(service, kIOMediaClass)) {
-				CFStringRef serialNumberAsCFString;
-				//CFStringEncoding encoding = kCFStringEncodingUTF8;
-				CFStringEncoding encoding = kCFStringEncodingMacRoman;
-
-
 				// Do we want to mount it yet?
 #if 1
-
-				serialNumberAsCFString = (CFStringRef*) IORegistryEntryCreateCFProperty(service, CFSTR("DOMOUNTME"), kCFAllocatorDefault, 0);
-				if (serialNumberAsCFString &&
-					CFEqual(serialNumberAsCFString, CFSTR("FALSE") )) {
+				cfstr = IORegistryEntryCreateCFProperty(service,
+				    CFSTR("DOMOUNTME"), kCFAllocatorDefault, 0);
+				if (cfstr && CFEqual(cfstr, CFSTR("FALSE"))) {
 					result = FSUR_UNRECOGNIZED;
+					CFRelease(cfstr);
 					IOObjectRelease(service);
-					syslog(LOG_NOTICE, "-zfs_probe : lying about it result %d", result);
+					syslog(LOG_NOTICE, "-zfs_probe : lying "
+					    "about it result %d", result);
 					return (result);
 				}
+				if (cfstr)
+					CFRelease(cfstr);
 #endif
-
-
-				serialNumberAsCFString = (CFStringRef*) IORegistryEntryCreateCFProperty(service, CFSTR("DATASET"), kCFAllocatorDefault, 0);
-
-				if (serialNumberAsCFString) {
-					*volname = CFStringGetCStringPtr(serialNumberAsCFString, encoding);
+				cfstr = IORegistryEntryCreateCFProperty(service,
+				    CFSTR("DATASET"), kCFAllocatorDefault, 0);
+				if (cfstr) {
+					(void) strlcpy(volname,
+					    CFStringGetCStringPtr(cfstr,
+					    kCFStringEncodingMacRoman),
+					    sizeof (io_name_t));
 					result = FSUR_RECOGNIZED;
+					CFRelease(cfstr);
 				}
-
-				syslog(LOG_NOTICE, "writing volname %s\n", *volname);
-
+				syslog(LOG_NOTICE, "writing volname %s\n",
+				    volname);
 			}
-            IOObjectRelease(service);
-        }
+			IOObjectRelease(service);
+		}
 	}
 
 	syslog(LOG_NOTICE, "-zfs_probe : result %d", result);
@@ -154,10 +161,10 @@ main(int argc, char **argv, char **env)
 {
 	setlogmask(LOG_UPTO(LOG_NOTICE));
 
-        int argindex;
-        for (argindex = 0; argindex < argc; argindex++) {
-                syslog(LOG_NOTICE, "argv[%d]: %s\n", argindex, argv[argindex]);
-        }
+	int argindex;
+	for (argindex = 0; argindex < argc; argindex++) {
+		syslog(LOG_NOTICE, "argv[%d]: %s\n", argindex, argv[argindex]);
+	}
 
 	char  blkdevice[MAXPATHLEN];
 	char  what;
@@ -165,7 +172,7 @@ main(int argc, char **argv, char **env)
 	char  *devname;
 	struct stat  sb;
 	int  ret = FSUR_INVAL;
-	char *volname = NULL;
+	io_name_t volname;
 
 	/* save & strip off program name */
 	progname = argv[0];
@@ -189,28 +196,29 @@ main(int argc, char **argv, char **env)
 	syslog(LOG_NOTICE, "blkdevice is %s", blkdevice);
 
 	if (stat(blkdevice, &sb) != 0) {
-		syslog(LOG_NOTICE, "%s: stat %s failed, %s", progname, blkdevice, strerror(errno));
+		syslog(LOG_NOTICE, "%s: stat %s failed, %s", progname,
+		    blkdevice, strerror(errno));
 		closelog();
 		exit(FSUR_INVAL);
 	}
 
 	switch (what) {
 		case FSUC_PROBE:
-			ret = zfs_probe(blkdevice, &volname);
+			ret = zfs_probe(blkdevice, volname);
 
 			if (ret == FSUR_RECOGNIZED)
 				write(1, volname, strlen(volname));
 			break;
 
 		case FSUC_GETUUID:
-			ret = zfs_probe(blkdevice, &volname);
+			ret = zfs_probe(blkdevice, volname);
 			if (ret == FSUR_RECOGNIZED) {
 				MD5_CTX  md5c;
 				unsigned char digest[MD5_DIGEST_LENGTH];
 				char uuidLine[40];
-				MD5_Init( &md5c );
-				MD5_Update( &md5c, volname, strlen(volname));
-				MD5_Final( digest, &md5c );
+				MD5_Init(&md5c);
+				MD5_Update(&md5c, volname, strlen(volname));
+				MD5_Final(digest, &md5c);
 				// 12962490-0DBE-3BCD-B22E-31B6CD7054E4
 				uuid_unparse(digest, uuidLine);
 				write(1, uuidLine, strlen(uuidLine));
@@ -220,7 +228,8 @@ main(int argc, char **argv, char **env)
 			break;
 
 		case FSUC_QUICKVERIFY:
-			syslog(LOG_NOTICE, "%s: returning OK on VERIFY", progname);
+			syslog(LOG_NOTICE, "%s: returning OK on VERIFY",
+			    progname);
 			ret = 0;
 			break;
 
