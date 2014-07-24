openzfsonosx/zfs

commit 8881f60a486ddb0295033ac01d36758b9594d128

Fixed wrong use of ZFS_OBJ_MUTEX() calls in some ASSERT() calls.

module/zfs/zfs_znode.c
@@ -609,7 +609,7 @@ zfs_znode_sa_init(zfsvfs_t *zfsvfs, znode_t *zp,
     dmu_buf_t *db, dmu_object_type_t obj_type, sa_handle_t *sa_hdl)
 {
 	ASSERT(!POINTER_IS_VALID(zp->z_zfsvfs) || (zfsvfs == zp->z_zfsvfs));
-	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zfsvfs, zp->z_id)));
+	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp)));
 
 	mutex_enter(&zp->z_lock);
 
@@ -640,7 +640,7 @@ zfs_znode_sa_init(zfsvfs_t *zfsvfs, znode_t *zp,
 void
 zfs_znode_dmu_fini(znode_t *zp)
 {
-	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp->z_zfsvfs, zp->z_id)) ||
+	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp)) ||
 	    zp->z_unlinked ||
 	    RW_WRITE_HELD(&zp->z_zfsvfs->z_teardown_inactive_lock));
 

commit b223a573025bb5ef84e6e08b74c9f24b61eacc0b

Fix zfs_znode_sa_init() panic by updating ZFS_OBJ_MUTEX()
The time at which a zp's z_zfsvfs is assigned was changed in
upstream here:

https://github.com/illumos/illumos-gate/commit/b5fca8f855054d167d04d3b4de5210c83ed2083c

Although we had already merged in most of that change, we had
neglected to update the definition of ZFS_OBJ_MUTEX() to the new
two-argument version. Yet, when actually invoking the macro, we
were supplying two arguments.

In an attempt to reconcile the discrepancy, the panic was
introduced by

https://github.com/openzfsonosx/zfs/commit/8881f60a486ddb0295033ac01d36758b9594d128

which erroneously reverted the new code where the two-argument
version was used, rather than updating ZFS_OBJ_MUTEX() to the new
two-argument version.

This results in panic because ZFS_OBJ_MUTEX() happens to be used
before zp->z_zfsvfs has been assigned, while the one-argument
version of ZFS_OBJ_MUTEX() expects to be able to retrieve the
zfsvfs from the zp.

include/sys/zfs_znode.h
@@ -310,9 +310,8 @@ typedef struct znode {
  * Macros for dealing with dmu_buf_hold
  */
 #define	ZFS_OBJ_HASH(obj_num)	((obj_num) & (ZFS_OBJ_MTX_SZ - 1))
-
-#define ZFS_OBJ_MUTEX(zp)       \
-        (&zp->z_zfsvfs->z_hold_mtx[ZFS_OBJ_HASH(zp->z_id)])
+#define	ZFS_OBJ_MUTEX(zfsvfs, obj_num)	\
+	(&(zfsvfs)->z_hold_mtx[ZFS_OBJ_HASH(obj_num)])
 #define ZFS_OBJ_HOLD_ENTER(zfsvfs, obj_num) \
         mutex_enter(&zfsvfs->z_hold_mtx[ZFS_OBJ_HASH(obj_num)])
 #define ZFS_OBJ_HELD(zfsvfs, obj_num) \

module/zfs/zfs_znode.c
@@ -624,7 +624,7 @@ zfs_znode_sa_init(zfsvfs_t *zfsvfs, znode_t *zp,
 				    sa_handle_t *sa_hdl)
 {
 	ASSERT(!POINTER_IS_VALID(zp->z_zfsvfs) || (zfsvfs == zp->z_zfsvfs));
-	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp)));
+	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zfsvfs, zp->z_id)));
 
 	mutex_enter(&zp->z_lock);
 
@@ -655,7 +655,7 @@ zfs_znode_sa_init(zfsvfs_t *zfsvfs, znode_t *zp,
 void
 zfs_znode_dmu_fini(znode_t *zp)
 {
-	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp)) ||
+	ASSERT(MUTEX_HELD(ZFS_OBJ_MUTEX(zp->z_zfsvfs, zp->z_id)) ||
 	    zp->z_unlinked ||
 	    RW_WRITE_HELD(&zp->z_zfsvfs->z_teardown_inactive_lock));
 