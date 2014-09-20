3ad80aa8e04445446fa9fad2243c4354762dd71d
Brendon Humphrey <brendon@vm-mac.local>
2014-09-20 15:57:50 -0700

    WIP: adjust min arc size. only release as much memory as the OS requests, rather than release 1MB when the OS says zero memory needed.

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -2745,8 +2745,8 @@ arc_reclaim_thread(void *dummy __unused)
             amount = kmem_num_pages_wanted() * PAGE_SIZE;
 #endif
 
-            if (!amount)
-                amount = 1024780;
+            // BGH if (!amount)
+            //    amount = 1024780;
 
             dprintf("ARC reclaim: %llu\n", amount);
 
