19e53663ad4bf55e3ffa2a60040448dcd6a4ad45
Jorgen Lundman <lundman@lundman.net>
2014-09-30 10:27:33 +0900

    Remove verbose debug message

--- module/zfs/zfs_vnops_osx_lib.c
+++ module/zfs/zfs_vnops_osx_lib.c
@@ -345,7 +345,7 @@ zfs_getattr_znode_unlocked(struct vnode *vp, vattr_t *vap)
                     osname,
                     MAXPATHLEN);
             VATTR_SET_SUPPORTED(vap, va_name);
-			printf("getattr root returning '%s'\n", vap->va_name);
+			dprintf("getattr root returning '%s'\n", vap->va_name);
         }
 	}
 
