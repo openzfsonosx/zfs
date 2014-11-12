openzfsonosx/zfs

commit 0fea07bb27822eb55cdb51bf515f819b124c162d

Silence debug prints Remove dead code

module/zfs/zfs_vnops_osx.c
@@ -1145,7 +1145,7 @@ osx_write_pages(objset_t *os, uint64_t object, uint64_t offset, uint64_t size,
 
 static int
 zfs_pageout(zfsvfs_t *zfsvfs, znode_t *zp, upl_t upl, vm_offset_t upl_offset,
-			offset_t off, size_t size, int flags, kcondvar_t *condvar)
+			offset_t off, size_t size, int flags)
 {
 	dmu_tx_t *tx;
 	rl_t *rl;
@@ -1223,7 +1223,6 @@ top:
 	if (off >= filesz) {
 		/* ignore all pages */
 		err = 0;
-		if (condvar) cv_signal(condvar);
 		goto out;
 	} else if (off + len > filesz) {
 #if 0
@@ -1320,22 +1319,6 @@ top:
 		zfs_log_write(zfsvfs->z_log, tx, TX_WRITE, zp, off, len, 0,
 		    NULL, NULL);
 	}
-
-
-	// Once we release parent, we can not access "upl" anymore.
-	// So mark it successful here, there are no more failure paths.
-	if (!(flags & UPL_NOCOMMIT)) {
-		ubc_upl_commit_range(upl, upl_offset, size,
-							 (UPL_COMMIT_CLEAR_DIRTY |
-							  UPL_COMMIT_FREE_ON_EMPTY));
-	}
-
-	// release parent, if we are async
-	if (condvar) {
-		//printf("ASync pageout, releasing parent and calling dmu_tx_commit\n");
-		cv_signal(condvar);
-	}
-
 	dmu_tx_commit(tx);
 
 out:
@@ -1349,48 +1332,12 @@ out:
 			    (UPL_ABORT_ERROR | UPL_ABORT_FREE_ON_EMPTY));
 	}
 exit:
-
-	// If we had error, wake up the waiter
-	if (err && condvar) cv_signal(condvar);
-
 	ZFS_EXIT(zfsvfs);
 	if (err)
 		printf("ZFS: pageout failed %d\n", err);
-	//if (condvar) printf("async pageout complete: %p\n", zp);
 	return (err);
 }
 
-
-struct pageout_struct {
-	znode_t *zp;
-	upl_t upl;
-	vm_offset_t upl_offset;
-	offset_t offset;
-	size_t len;
-	int flags;
-	kmutex_t mutex;
-	kcondvar_t condvar;
-};
-typedef struct pageout_struct pageout_t;
-
-void
-vnop_pageout_async(void *arg)
-{
-	pageout_t *cb = (pageout_t *)arg;
-
-	// Make sure parent is waiting, mutex is released when cv_wait is called.
-	mutex_enter(&cb->mutex);
-	mutex_exit(&cb->mutex);
-
-	zfs_pageout(cb->zp->z_zfsvfs,
-				cb->zp, cb->upl,
-				cb->upl_offset,
-				cb->offset, cb->len,
-				cb->flags,
-				&cb->condvar);
-
-	thread_exit();
-}
 
 
 static int
@@ -1443,45 +1390,14 @@ zfs_vnop_pageout(struct vnop_pageout_args *ap)
 	 * and we launch another to handle this pageout request
 	 */
 	if (zfsvfs->z_vnode_create_depth) {
-		pageout_t *cb;
-
 		if (!(flags & UPL_NOCOMMIT))
 			(void) ubc_upl_abort(upl, UPL_ABORT_UNAVAILABLE);
 		return EIO;
 
-
-		dprintf("ZFS: async pageout request: %p\n", zp);
-
-		cb = kmem_alloc(sizeof(*cb), KM_SLEEP);
-		mutex_init(&cb->mutex, NULL, MUTEX_DEFAULT, NULL);
-		cv_init(&cb->condvar, NULL, CV_DEFAULT, NULL);
-
-		mutex_enter(&cb->mutex);
-
-		cb->upl = upl;
-		cb->zp = zp;
-		cb->upl_offset = upl_offset;
-		cb->offset = ap->a_f_offset;
-		cb->len = len;
-		cb->flags = flags;
-
-		(void) thread_create(NULL, 0, vnop_pageout_async, cb, 0, &p0,
-							 TS_RUN, minclsyspri);
-
-		// Wait thread to start and get to dmu_tx_sync, then we resume here.
-		cv_wait(&cb->condvar, &cb->mutex);
-		dprintf("Parent pageout woke: zp %p\n", zp);
-
-		mutex_exit(&cb->mutex);
-		mutex_destroy(&cb->mutex);
-		cv_destroy(&cb->condvar);
-		kmem_free(cb, sizeof(*cb));
-		return 0;
-
 	}
 
 	return zfs_pageout(zfsvfs, zp, upl, upl_offset, ap->a_f_offset,
-					   len, flags, NULL);
+					   len, flags);
 
 }
 

module/zfs/arc.c
@@ -2505,7 +2505,7 @@ arc_reclaim_thread(void *notused)
 
         if (arc_reclaim_needed()) {
 
-			printf("ZFS: arc reclaim needed: last %u nogrow %u\n", last_reclaim,
+			dprintf("ZFS: arc reclaim needed: last %u nogrow %u\n", last_reclaim,
 				   arc_no_grow);
 
             if (arc_no_grow) {

module/zfs/zfs_vfsops.c
@@ -1382,7 +1382,7 @@ zfsvfs_free(zfsvfs_t *zfsvfs)
 	while(!list_empty(&zfsvfs->z_all_znodes) ||
 		  !list_empty(&zfsvfs->z_reclaim_znodes)) {
 		cv_signal(&zfsvfs->z_reclaim_thr_cv);
-		printf("Waiting for reclaim to drain: %d + %d\n",
+		printf("ZFS: Waiting for reclaim to drain: %d + %d\n",
 			   list_empty(&zfsvfs->z_all_znodes),
 			   list_empty(&zfsvfs->z_reclaim_znodes));
 		delay(hz);
@@ -1396,7 +1396,7 @@ zfsvfs_free(zfsvfs_t *zfsvfs)
 	while (zfsvfs->z_reclaim_thread_exit == TRUE)
 		cv_wait(&zfsvfs->z_reclaim_thr_cv, &zfsvfs->z_reclaim_thr_lock);
 	mutex_exit(&zfsvfs->z_reclaim_thr_lock);
-	printf("Complete\n");
+	dprintf("Complete\n");
 
 	mutex_destroy(&zfsvfs->z_reclaim_thr_lock);
 	cv_destroy(&zfsvfs->z_reclaim_thr_cv);
@@ -2434,7 +2434,7 @@ zfsvfs_teardown(zfsvfs_t *zfsvfs, boolean_t unmounting)
 	while(!list_empty(&zfsvfs->z_all_znodes) ||
 		  !list_empty(&zfsvfs->z_reclaim_znodes)) {
 		cv_signal(&zfsvfs->z_reclaim_thr_cv);
-		printf("Waiting for reclaim to drain: %d + %d\n",
+		printf("ZFS:Waiting for reclaim to drain: %d + %d\n",
 			   list_empty(&zfsvfs->z_all_znodes),
 			   list_empty(&zfsvfs->z_reclaim_znodes));
 		delay(hz);
@@ -2704,7 +2704,7 @@ zfs_vfs_unmount(struct mount *mp, int mntflags, vfs_context_t context)
 		/*
 		 * Finally release the objset
 		 */
-        printf("disown\n");
+        dprintf("disown\n");
 		dmu_objset_disown(os, zfsvfs);
 	}
 
@@ -3155,6 +3155,8 @@ zfs_freevfs(struct mount *vfsp)
 #endif	/* sun */
 
 	zfsvfs_free(zfsvfs);
+
+	vfs_setfsprivate(vfsp, NULL);
 
 	atomic_add_32(&zfs_active_fs_count, -1);
     dprintf("-freevfs\n");

commit 3793db0c5f9a84928d4c24ef6f8b9cbc36e1c1ab

Commit 0fea07bb27822eb55cdb51bf515f819b124c162d accidentally also removed the call to upl_commit, leading to leak and deadlock.

module/zfs/zfs_vnops_osx.c
@@ -1153,14 +1153,15 @@ zfs_pageout(zfsvfs_t *zfsvfs, znode_t *zp, upl_t upl, vm_offset_t upl_offset,
 	int err = 0;
 	size_t len = size;
 
-	dprintf("+vnop_pageout: off 0x%llx len 0x%lx upl_off 0x%lx: "
+	printf("+vnop_pageout: off 0x%llx len 0x%lx upl_off 0x%lx: "
 	    "blksz 0x%x, z_size 0x%llx upl %p\n",
 		   off, len, upl_offset, zp->z_blksz,
 		   zp->z_size, upl);
 
-	if (upl == (upl_t)NULL)
+	if (upl == (upl_t)NULL) {
+		printf("ZFS: vnop_pageout: failed on NULL upl\n");
 		return EINVAL;
-
+	}
 	/*
 	 * We can't leave this function without either calling upl_commit or
 	 * upl_abort. So use the non-error version.
@@ -1168,7 +1169,8 @@ zfs_pageout(zfsvfs_t *zfsvfs, znode_t *zp, upl_t upl, vm_offset_t upl_offset,
 	ZFS_ENTER_NOERROR(zfsvfs);
 	if (zfsvfs->z_unmounted) {
 		if (!(flags & UPL_NOCOMMIT))
-			(void) ubc_upl_abort(upl, UPL_ABORT_UNAVAILABLE);
+			(void) ubc_upl_abort(upl, UPL_ABORT_DUMP_PAGES|UPL_ABORT_FREE_ON_EMPTY);
+		printf("ZFS: vnop_pageout: abort on z_unmounted\n");
 		ZFS_EXIT(zfsvfs);
 		return EIO;
 	}
@@ -1179,7 +1181,7 @@ zfs_pageout(zfsvfs_t *zfsvfs, znode_t *zp, upl_t upl, vm_offset_t upl_offset,
 
 	if (len <= 0) {
 		if (!(flags & UPL_NOCOMMIT))
-			(void) ubc_upl_abort(upl, UPL_ABORT_ERROR);
+			(void) ubc_upl_abort(upl, UPL_ABORT_DUMP_PAGES|UPL_ABORT_FREE_ON_EMPTY);
 		err = EINVAL;
 		goto exit;
 	}
@@ -1330,6 +1332,10 @@ out:
 		if (err)
 			ubc_upl_abort_range(upl, upl_offset, size,
 			    (UPL_ABORT_ERROR | UPL_ABORT_FREE_ON_EMPTY));
+		else
+			ubc_upl_commit_range(upl, upl_offset, size,
+								 (UPL_COMMIT_CLEAR_DIRTY |
+								  UPL_COMMIT_FREE_ON_EMPTY));
 	}
 exit:
 	ZFS_EXIT(zfsvfs);
@@ -1366,7 +1372,7 @@ zfs_vnop_pageout(struct vnop_pageout_args *ap)
 	if (!zp || !zp->z_zfsvfs) {
 		if (!(flags & UPL_NOCOMMIT))
 			ubc_upl_abort(upl,
-			    (UPL_ABORT_UNAVAILABLE));
+			    (UPL_ABORT_DUMP_PAGES|UPL_ABORT_FREE_ON_EMPTY));
 		printf("ZFS: vnop_pageout: null zp or zfsvfs\n");
 		return (ENXIO);
 	}
@@ -1391,7 +1397,8 @@ zfs_vnop_pageout(struct vnop_pageout_args *ap)
 	 */
 	if (zfsvfs->z_vnode_create_depth) {
 		if (!(flags & UPL_NOCOMMIT))
-			(void) ubc_upl_abort(upl, UPL_ABORT_UNAVAILABLE);
+			(void) ubc_upl_abort(upl, UPL_ABORT_DUMP_PAGES|UPL_ABORT_FREE_ON_EMPTY);
+		printf("ZFS: vnop_pageout: abort on vnode_create\n");
 		return EIO;
 
 	}