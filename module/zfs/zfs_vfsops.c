18a9dda06ce34f09df4587189803fa32ae053725
Jorgen Lundman <lundman@lundman.net>
2014-11-30 10:30:17 +0900

    Ensure all zfs_rmnode goes through reclaim thread
    vnode_create can be called by hfs, and we end up calling zfs_rmnode
    which in turn calls zget, which sometimes can call vnode_getwithvid()
    causing deadlock. Now we channel ALL zfs_rmnode into the separate
    reclaim_thread.

--- module/zfs/zfs_vfsops.c
+++ module/zfs/zfs_vfsops.c
@@ -1258,14 +1258,11 @@ zfsvfs_create(const char *osname, zfsvfs_t **zfvp)
 	mutex_init(&zfsvfs->z_lock, NULL, MUTEX_DEFAULT, NULL);
 	mutex_init(&zfsvfs->z_reclaim_list_lock, NULL, MUTEX_DEFAULT, NULL);
 	mutex_init(&zfsvfs->z_reclaim_thr_lock, NULL, MUTEX_DEFAULT, NULL);
-	mutex_init(&zfsvfs->z_reentry_lock, NULL, MUTEX_DEFAULT, NULL);
 	cv_init(&zfsvfs->z_reclaim_thr_cv, NULL, CV_DEFAULT, NULL);
 	list_create(&zfsvfs->z_all_znodes, sizeof (znode_t),
 	    offsetof(znode_t, z_link_node));
 	list_create(&zfsvfs->z_reclaim_znodes, sizeof (znode_t),
 	    offsetof(znode_t, z_link_reclaim_node));
-	list_create(&zfsvfs->z_reentry_threads, sizeof (reentry_threads_t),
-	    offsetof(reentry_threads_t, reentry_node));
 	rrw_init(&zfsvfs->z_teardown_lock, B_FALSE);
 	rw_init(&zfsvfs->z_teardown_inactive_lock, NULL, RW_DEFAULT, NULL);
 	rw_init(&zfsvfs->z_fuid_lock, NULL, RW_DEFAULT, NULL);
@@ -1410,10 +1407,8 @@ zfsvfs_free(zfsvfs_t *zfsvfs)
 	mutex_destroy(&zfsvfs->z_znodes_lock);
 	mutex_destroy(&zfsvfs->z_lock);
 	mutex_destroy(&zfsvfs->z_reclaim_list_lock);
-	mutex_destroy(&zfsvfs->z_reentry_lock);
 	list_destroy(&zfsvfs->z_all_znodes);
 	list_destroy(&zfsvfs->z_reclaim_znodes);
-	list_destroy(&zfsvfs->z_reentry_threads);
 	rrw_destroy(&zfsvfs->z_teardown_lock);
 	rw_destroy(&zfsvfs->z_teardown_inactive_lock);
 	rw_destroy(&zfsvfs->z_fuid_lock);
