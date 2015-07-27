3dcb9ff97e0ff5f19a8f07acbd8e90ff33bd966e
Evan Susarret <evansus@gmail.com>
2015-07-26 22:53:44 -0400

    When taking a snapshot, call zvol_create_minor.
    As opposed to zvol_create_minors, which scans for all zvols below
    the named dataset.
    This is already called for each snapshot that is created, so the
    previous call just added overhead.

--- module/zfs/dsl_dataset.c
+++ module/zfs/dsl_dataset.c
@@ -1453,7 +1453,7 @@ dsl_dataset_snapshot(nvlist_t *snaps, nvlist_t *props, nvlist_t *errors)
 		for (pair = nvlist_next_nvpair(snaps, NULL); pair != NULL;
 		    pair = nvlist_next_nvpair(snaps, pair)) {
 			char *snapname = nvpair_name(pair);
-			zvol_create_minors(snapname);
+			zvol_create_minor(snapname);
 		}
 	}
 #endif
