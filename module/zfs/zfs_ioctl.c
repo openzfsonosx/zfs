e45f37a9820c25392094bf7c1a80e3f81ac743d0
Jorgen Lundman <lundman@lundman.net>
2015-09-01 11:27:58 +0900

    Protect the zfsdev_state_list properly
    Grabbing the zfsdev_state_lock was missed in a few places, which
    protect the zfsdev_state_list.

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -5736,12 +5736,13 @@ zfsdev_open(dev_t dev, int flags, int devtype, struct proc *p)
 	dprintf("zfsdev_open, dev %d flag %02X devtype %d, proc is %p: thread %p\n",
 			minor(dev), flags, devtype, p, current_thread());
 
+	mutex_enter(&zfsdev_state_lock);
 	if (zfsdev_get_state_impl(minor(dev), ZST_ALL)) {
+		mutex_exit(&zfsdev_state_lock);
 		dprintf("zs already exists\n");
 		return (0);
 	}
 
-	mutex_enter(&zfsdev_state_lock);
 	error = zfsdev_state_init(dev);
 	mutex_exit(&zfsdev_state_lock);
 
@@ -5796,10 +5797,13 @@ zfsdev_ioctl(dev_t dev, u_long cmd, caddr_t arg,  __unused int xflag, struct pro
 	}
 #endif
 
+	mutex_enter(&zfsdev_state_lock);
 	if (zfsdev_get_state_impl(minorx, ZST_ALL) == NULL) {
+		mutex_exit(&zfsdev_state_lock);
 		dprintf("Calling zvol ioctl minor %d \n", minorx);
 		return (zvol_ioctl(dev, cmd, arg, 0, NULL, NULL));
 	}
+	mutex_exit(&zfsdev_state_lock);
 
 	vecnum = cmd - ZFS_IOC_FIRST;
 #ifdef illumos
