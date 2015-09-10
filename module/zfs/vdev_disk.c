0ee7b4b7633c183c2071358bc06f469853cb442e
rottegift <aguestpunk@cla.use.net>
2015-09-10 08:58:23 +0100

    strstr not in kernel

--- module/zfs/vdev_disk.c
+++ module/zfs/vdev_disk.c
@@ -125,13 +125,40 @@ const char *devids[] = {
 NULL	
 };
 
+// from http://www.opensource.apple.com/source/xnu/xnu-792.13.8/libsa/strstr.c
+
+static inline char *
+smd_strstr(const char *in, const char *str)
+{
+  char c;
+  size_t len;
+
+  c = *str++;
+  if (!c)
+    return (char *) in;	// Trivial empty string case
+
+  len = strlen(str);
+  do {
+    char sc;
+
+    do {
+      sc = *in++;
+      if (!sc)
+	return (char *) 0;
+    } while (sc != c);
+  } while (strncmp(in, str, len) != 0);
+
+  return (char *) (in - 1);
+}
+
+
 static inline int
-ssd_search(char a[]) {
+ssd_search(const char a[]) {
   int i;
   char *p = NULL;
 
   for(i=0; devids[i] != NULL; i++) {
-    if((p=strstr(a, devids[i]))!=NULL) {
+    if((p=smd_strstr(a, devids[i]))!=NULL) {
       printf("ZFS: smd: issid: %s\n", a);
       return 1;
     }
