fec2339b7e93186728cfcdfc110593431ae6b22c
Jorgen Lundman <lundman@lundman.net>
2015-12-05 07:59:27 +0900

    Ensure pool's txg is commited for accurate space stats
    This addresses part 1 of the Finder issue #436

--- module/zfs/zfs_vfsops.c
+++ module/zfs/zfs_vfsops.c
@@ -2182,11 +2182,7 @@ zfs_vfs_getattr(struct mount *mp, struct vfs_attr *fsap, __unused vfs_context_t 
 
 	ZFS_ENTER(zfsvfs);
 
-	/* Finder will show the old/incorrect size, we can force a sync of the pool
-	 * to make it correct, but that has side effects which are undesirable.
-	 */
-	/* txg_wait_synced(dmu_objset_pool(zfsvfs->z_os), 0); */
-
+	txg_wait_synced(dmu_objset_pool(zfsvfs->z_os), 0);
 	dmu_objset_space(zfsvfs->z_os,
 	    &refdbytes, &availbytes, &usedobjs, &availobjs);
 
