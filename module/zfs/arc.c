e90a0f0b755a64e7749482c30f0efeaba8dc517b
ilovezfs <ilovezfs@icloud.com>
2016-01-20 04:24:48 -0800

    Revert "Conservative ARC 1/4 or 3GB"
    This reverts commit ae79d84688ffcb77fcc18ad77c7f1eb71e179b8d.
    
    The reduction didn't take into account that allmem is already only
    eighty percent of the real total physical memory due to SPL's
    sequestration of the rest in openzfsonosx/spl@373e40b.

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -5278,12 +5278,12 @@ arc_init(void)
 
 	/* set min cache to 1/32 of all memory, or 64MB, whichever is more */
 	arc_c_min = MAX(allmem / 32, 64 << 20);
-	/* set max to 1/4 of all memory, or all but 3GB, whichever is more */
-	if (allmem >= 3ULL << 30)
-		arc_c_max = allmem - (3ULL << 30);
+	/* set max to 3/4 of all memory, or all but 1GB, whichever is more */
+	if (allmem >= 1 << 30)
+		arc_c_max = allmem - (1 << 30);
 	else
 		arc_c_max = arc_c_min;
-	arc_c_max = MAX(allmem * 1 / 4, arc_c_max);
+	arc_c_max = MAX(allmem * 3 / 4, arc_c_max);
 
 	/*
 	 * In userland, there's only the memory pressure that we artificially
