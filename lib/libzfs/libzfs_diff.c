3a7b2c3a48ca14180bfa7116fa292b72dced6253
Joshua M. Clulow <josh@sysmgr.org>
2016-01-30 21:20:58 +0100

    Illumos 4448 - zfs diff misprints unicode characters
    4448 zfs diff misprints unicode characters
    Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com>
    Reviewed by: Toomas Soome <tsoome@me.com>
    Approved by: Matthew Ahrens <mahrens@delphix.com>
    
    References:
      https://www.illumos.org/issues/4448
      https://github.com/illumos/illumos-gate/commit/b211eb9
    
    Porting Notes:
    - [lib/libzfs/libzfs_diff.c]
      - 38145d6 Ensure that zfs diff prints unicode safely.
      - 141b638 Change 3-digit octal escapes to 4-digit ones
    
    Ported-by: kernelOfTruth kerneloftruth@gmail.com
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- lib/libzfs/libzfs_diff.c
+++ lib/libzfs/libzfs_diff.c
@@ -22,7 +22,7 @@
 /*
  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.
- * Copyright 2015 Joyent, Inc.
+ * Copyright 2016 Joyent, Inc.
  */
 
 /*
