c6f5169f8527b670edf054f7c05ebf33c22dd964
Jorgen Lundman <lundman@lundman.net>
2016-02-17 14:35:11 +0900

    Revert "Update Finder on zfs rollback"
    This reverts commit ee3b23f4264503dc60902189be7b400217c65088.
    
    As it only makes Finder partially refresh, it left many desirable side effects.
    Replaced with alternate method.

--- include/sys/zfs_vfsops.h
+++ include/sys/zfs_vfsops.h
@@ -245,7 +245,6 @@ extern void zfs_freevfs(struct mount *vfsp);
 extern int  zfsvfs_create(const char *osname, zfsvfs_t **zfvp);
 extern void zfsvfs_free(zfsvfs_t *zfsvfs);
 
-extern void zfs_findernotify_now(zfsvfs_t *zfsvfs);
 
 #ifdef	__cplusplus
 }
