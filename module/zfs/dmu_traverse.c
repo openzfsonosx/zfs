7d9363f18798d77c83f0497ba165bbe20551d6cb
Paul Dagnelie <pcd@delphix.com>
2016-03-09 09:11:56 -0800

    6370 ZFS send fails to transmit some holes
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Chris Williamson <chris.williamson@delphix.com>
    Reviewed by: Stefan Ring <stefanrin@gmail.com>
    Reviewed by: Steven Burgess <sburgess@datto.com>
    Reviewed by: Arne Jansen <sensille@gmx.net>
    Approved by: Robert Mustacchi <rm@joyent.com>
    
    In certain circumstances, "zfs send -i" (incremental send) can produce a
    stream which will result in incorrect sparse file contents on the
    target.
    
    The problem manifests as regions of the received file that should be
    sparse (and read a zero-filled) actually contain data from a file that
    was deleted (and which happened to share this file's object ID).
    
    Note: this can happen only with filesystems (not zvols, because they do
    not free (and thus can not reuse) object IDs).
    
    Note: This can happen only if, since the incremental source (FromSnap),
    a file was deleted and then another file was created, and the new file
    is sparse (i.e. has areas that were never written to and should be
    implicitly zero-filled).
    
    We suspect that this was introduced by 4370 (applies only if hole_birth
    feature is enabled), and made worse by 5243 (applies if hole_birth
    feature is disabled, and we never send any holes).
    
    The bug is caused by the hole birth feature. When an object is deleted
    and replaced, all the holes in the object have birth time zero. However,
    zfs send cannot tell that the holes are new since the file was replaced,
    so it doesn't send them in an incremental. As a result, you can end up
    with invalid data when you receive incremental send streams. As a
    short-term fix, we can always send holes with birth time 0 (unless it's
    a zvol or a dataset where we can guarantee that no objects have been
    reused).

--- module/zfs/dmu_traverse.c
+++ module/zfs/dmu_traverse.c
@@ -252,8 +252,7 @@ traverse_visitbp(traverse_data_t *td, const dnode_phys_t *dnp,
 		 *
 		 * Note that the meta-dnode cannot be reallocated.
 		 */
-		if (!send_holes_without_birth_time &&
-		    (!td->td_realloc_possible ||
+		if ((!td->td_realloc_possible ||
 		    zb->zb_object == DMU_META_DNODE_OBJECT) &&
 		    td->td_hole_birth_enabled_txg <= td->td_min_txg)
 			return (0);
@@ -361,6 +360,15 @@ traverse_visitbp(traverse_data_t *td, const dnode_phys_t *dnp,
 
 		prefetch_dnode_metadata(td, &osp->os_meta_dnode, zb->zb_objset,
 			DMU_META_DNODE_OBJECT);
+		/*
+		 * See the block comment above for the goal of this variable.
+		 * If the maxblkid of the meta-dnode is 0, then we know that
+		 * we've never had more than DNODES_PER_BLOCK objects in the
+		 * dataset, which means we can't have reused any object ids.
+		 */
+		if (osp->os_meta_dnode.dn_maxblkid == 0)
+			td->td_realloc_possible = B_FALSE;
+
 		if (arc_buf_size(buf) >= sizeof (objset_phys_t)) {
 			prefetch_dnode_metadata(td, gdnp, zb->zb_objset,
 			    DMU_GROUPUSED_OBJECT);
