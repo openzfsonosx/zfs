5c1648d8663aa4821678e13c7a9b81c8a8f00378
Alexander Motin <mav@FreeBSD.org>
2016-04-09 20:16:05 -0700

    6322 ZFS indirect block predictive prefetch
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Paul Dagnelie <pcd@delphix.com>
    
    Improve speculative prefetch of indirect blocks.
    
    Scalability of many operations on wide ZFS pool can be limited by
    requirement to prefetch indirect blocks first.  Recently added
    asynchronous indirect block read partially helped, but did not
    solve the problem completely.  This patch extends existing prefetcher
    functionality to explicitly work with indirect blocks.
    
    Before this change prefetcher issued reads for up to 8MB of data in
    advance.  With this change it also issues indirect block reads
    for up to 64MB of data in advance, so that when it will be time to
    actually read those data, it can be done immediately.  Alike effect
    can be achieved by just increasing maximal data prefetch distance,
    but at higher memory cost.
    
    Also this change introduces indirect block prefetch for rewrite
    operations, that was never done before.  Previously ARC miss for
    indirect blocks regularly blocked rewrites, converting perfectly
    aligned asynchronous operations into synchronous read-write pairs,
    significantly reducing maximal rewrite speed.
    
    While being there this issue was also fixed:
     - prefetch was done always, even if caching for the dataset was
    completely disabled.
    
    Testing on FreeBSD with zvol on top of 6x striped 2x mirrored pool
    of 12 assorted HDDs shown me such performance numbers:
    ------- BEFORE --------
    Write	491363677 bytes/sec
    Read	312430631 bytes/sec
    Rewrite  97680464 bytes/sec
    -------- AFTER --------
    Write	493524146 bytes/sec
    Read	438598079 bytes/sec
    Rewrite	277506044 bytes/sec

--- include/sys/dnode.h
+++ include/sys/dnode.h
@@ -329,6 +329,15 @@ kmem_cbrc_t
 dnode_move(void *buf, void *newbuf, size_t size, void *arg);
 void dnode_evict_bonus(dnode_t *dn);
 
+#define	DNODE_IS_CACHEABLE(_dn)						\
+	((_dn)->dn_objset->os_primary_cache == ZFS_CACHE_ALL ||		\
+	(DMU_OT_IS_METADATA((_dn)->dn_type) &&				\
+	(_dn)->dn_objset->os_primary_cache == ZFS_CACHE_METADATA))
+
+#define	DNODE_META_IS_CACHEABLE(_dn)					\
+	((_dn)->dn_objset->os_primary_cache == ZFS_CACHE_ALL ||		\
+	(_dn)->dn_objset->os_primary_cache == ZFS_CACHE_METADATA)
+
 #ifdef ZFS_DEBUGXXX
 
 /*
