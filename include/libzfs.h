4d50b2ad0b8a267f0851dd4a8ae17b451a1dcc40
Martin Matuska <martin@matuska.org>
2016-05-15 06:37:43 -0700

    6902 speed up listing of snapshots if requesting name only and sorting by name
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Pawel Dawidek <pjd@freebsd.org>
    Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov>
    
    Dramatically optimize listing snapshots when user requests only snapshot
    names and wants to sort them by name, ie. when executes:
    
    	# zfs list -t snapshot -o name -s name
    
    Because only name is needed we don't have to read all snapshot properties.
    
    Below you can find how long does it take to list 34509 snapshots from a single
    disk pool before and after this change with cold and warm cache:
    
    before:
    
    	# time zfs list -t snapshot -o name -s name > /dev/null
    	cold cache: 525s
    	warm cache: 218s
    
    after:
    
    	# time zfs list -t snapshot -o name -s name > /dev/null
    	cold cache: 1.7s
    	warm cache: 1.1s
    
    OSX porting comments: We had most of this commit already in our code base
    which was unexpected.

--- include/libzfs.h
+++ include/libzfs.h
@@ -21,6 +21,7 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011 Pawel Jakub Dawidek. All rights reserved.
  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
  * Copyright (c) 2012, Joyent, Inc. All rights reserved.
  * Copyright (c) 2013 Steven Hartland. All rights reserved.
