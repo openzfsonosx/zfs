2080d83c08ce6df20101f654a2aaa38865496bb2
Evan Susarret <evansus@gmail.com>
2016-06-28 06:27:04 -0400

    Add back missing spa_async_dispatch

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -6071,6 +6071,7 @@ spa_async_request(spa_t *spa, int task)
 	mutex_enter(&spa->spa_async_lock);
 	spa->spa_async_tasks |= task;
 	mutex_exit(&spa->spa_async_lock);
+	spa_async_dispatch(spa);
 }
 
 /*
