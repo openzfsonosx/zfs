72472deaec7897e8bf917b3a52439122d611b048
Jorgen Lundman <lundman@lundman.net>
2016-07-08 09:19:43 +0900

    Allow bootfs and recordsize=1m to coexist
    This is a limitation of GRUB and does not apply to OSX port.

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -519,12 +519,14 @@ spa_prop_validate(spa_t *spa, nvlist_t *props)
 				    &propval)) == 0 &&
 				    !BOOTFS_COMPRESS_VALID(propval)) {
 					error = SET_ERROR(ENOTSUP);
+#ifndef __APPLE__ /* OSX can boot large recordsize just fine */
 				} else if ((error =
 				    dsl_prop_get_int_ds(dmu_objset_ds(os),
 				    zfs_prop_to_name(ZFS_PROP_RECORDSIZE),
 				    &propval)) == 0 &&
 				    propval > SPA_OLD_MAXBLOCKSIZE) {
 					error = SET_ERROR(ENOTSUP);
+#endif
 				} else {
 					objnum = dmu_objset_id(os);
 				}
