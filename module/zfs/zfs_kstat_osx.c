b69fd3130a3599367a116d9f24c6eef7ddf8707e
rottegift <rottegift@mac.com>
2016-08-30 11:39:46 +0100

    smd: track number of dbuf_redirty() calls

--- module/zfs/zfs_kstat_osx.c
+++ module/zfs/zfs_kstat_osx.c
@@ -171,6 +171,7 @@ osx_kstat_t osx_kstat = {
 	{"zfs_write_implies_delete_child",KSTAT_DATA_UINT64  },
 	{"zfs_send_holes_without_birth_time",KSTAT_DATA_UINT64  },
 
+	{"zfs_redirtied_blocks",KSTAT_DATA_UINT64  },
 };
 
 
@@ -541,6 +542,8 @@ static int osx_kstat_update(kstat_t *ksp, int rw)
 			zfs_write_implies_delete_child;
 		ks->zfs_send_holes_without_birth_time.value.ui64 =
 			send_holes_without_birth_time;
+
+		ks->zfs_redirtied_blocks.value.ui64 = zfs_redirtied_blocks;
 	}
 
 	return 0;
