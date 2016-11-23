a4291d02fdc807a08fe9e3229ee0ff15ebe36b2d
George Wilson <george.wilson@delphix.com>
2016-11-23 09:06:28 -0600

    7303 dynamic metaslab selection
    Reviewed by: Alex Reece <alex@delphix.com>
    Reviewed by: Chris Siden <christopher.siden@delphix.com>
    Reviewed by: Dan Kimmel <dan.kimmel@delphix.com>
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Paul Dagnelie <paul.dagnelie@delphix.com>
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Reviewed by: Don Brady <don.brady@intel.com>
    
    Porting notes: Requires SPL commit dc362c6a7cbd

--- include/sys/zfs_debug.h
+++ include/sys/zfs_debug.h
@@ -49,14 +49,15 @@ extern int zfs_flags;
 extern int zfs_recover;
 extern int zfs_free_leak_on_eio;
 
-#define	ZFS_DEBUG_DPRINTF		(1<<0)
-#define	ZFS_DEBUG_DBUF_VERIFY		(1<<1)
-#define	ZFS_DEBUG_DNODE_VERIFY		(1<<2)
-#define	ZFS_DEBUG_SNAPNAMES		(1<<3)
-#define	ZFS_DEBUG_MODIFY		(1<<4)
-#define	ZFS_DEBUG_SPA			(1<<5)
-#define	ZFS_DEBUG_ZIO_FREE		(1<<6)
-#define	ZFS_DEBUG_HISTOGRAM_VERIFY	(1<<7)
+#define	ZFS_DEBUG_DPRINTF		(1 << 0)
+#define	ZFS_DEBUG_DBUF_VERIFY		(1 << 1)
+#define	ZFS_DEBUG_DNODE_VERIFY		(1 << 2)
+#define	ZFS_DEBUG_SNAPNAMES		(1 << 3)
+#define	ZFS_DEBUG_MODIFY		(1 << 4)
+#define	ZFS_DEBUG_SPA			(1 << 5)
+#define	ZFS_DEBUG_ZIO_FREE		(1 << 6)
+#define	ZFS_DEBUG_HISTOGRAM_VERIFY	(1 << 7)
+#define	ZFS_DEBUG_METASLAB_VERIFY	(1 << 8)
 
 /*
  * Always log zfs debug messages to the spl debug subsystem as SS_USER1.
