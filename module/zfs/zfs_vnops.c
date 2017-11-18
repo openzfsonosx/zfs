24baae0547d03649be701a274d7a3cb18f76de51
Sean Doran <smd@use.net>
2017-11-18 00:18:14 +0000

    do not zero if we are at page boundary

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -776,7 +776,8 @@ fill_hole(vnode_t *vp, const off_t foffset,
 	const off_t upl_first_page = trunc_page_64(upl_start) / PAGE_SIZE_64; // absolute-in-file
 	const off_t upl_last_page = page_hole_end - page_hole_start; // relative-to-upl
 
-	if (upl_last_page >= eof_page && upl_first_page <= eof_page) {
+	if (upl_last_page >= eof_page && upl_first_page <= eof_page &&
+	    ((eof_byte % PAGE_SIZE_64) != 0)) {
 		ASSERT3U(upl_first_page, <=, eof_page);
 		printf("ZFS: %s:%d page range [%lld - %lld] contains eof page %lld (eof byte %lld)\n",
 		    __func__, __LINE__,
