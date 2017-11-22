023ba0cda78d3e476da435ef9f8a22c712d3cf57
Sean Doran <smd@use.net>
2017-11-22 19:39:23 +0000

    fix assertion

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -1175,7 +1175,7 @@ mappedread_new(vnode_t *vp, int arg_bytes, struct uio *uio)
 	// size of the UPL, page-aligned bytes
 	const size_t upl_size = round_page_64(inbytes + upl_sz_vs_fsize);
 
-	ASSERT3S(upl_file_offset + upl_size, <=, zp->z_size);
+	ASSERT3S(upl_file_offset + upl_size, <=, round_page_64(zp->z_size));
 
 	err = fill_holes_in_range(vp, upl_file_offset, upl_size);
 
