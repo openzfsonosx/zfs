b89fcb8b1c5d114a19885372e7e017196dae4465
Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
2019-04-20 04:03:32 +0900

    Fix incorrect "[UNUSED]" comments
    These aren't unused.
    `flag` in zfs_create() also isn't to indicate large file.
    
    Reviewed-by: Richard Elling <Richard.Elling@RichardElling.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: George Melikov <mail@gmelikov.ru>
    Reviewed-by: Richard Laager <rlaager@wiktel.com>
    Signed-off-by: Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
    Closes #8635

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -1616,7 +1616,7 @@ zfs_lookup(vnode_t *dvp, char *nm, vnode_t **vpp, struct componentname *cnp,
  *		excl	- flag indicating exclusive or non-exclusive mode.
  *		mode	- mode to open file with.
  *		cr	- credentials of caller.
- *		flag	- large file flag [UNUSED].
+ *		flag	- file flag.
  *		ct	- caller context
  *		vsecp 	- ACL to be set
  *
@@ -5622,7 +5622,7 @@ zfs_delmap(vnode_t *vp, offset_t off, struct as *as, caddr_t addr,
  *		bfp	- section of file to free/alloc.
  *		flag	- current file open mode flags.
  *		offset	- current file offset.
- *		cr	- credentials of caller [UNUSED].
+ *		cr	- credentials of caller.
  *		ct	- caller context.
  *
  *	RETURN:	0 on success, error code on failure.
